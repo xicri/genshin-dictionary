cce2f71bb015209c87ffe7c5ea6ef6e98441d81b
Xicri <xicri@proton.me>
2022-07-09 14:53:00 +0900

    chore: allow loading dataset from local genshin-langdata repository

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -1,7 +1,8 @@
-import { mkdir, writeFile } from "fs/promises";
+import { copyFile, mkdir, writeFile } from "fs/promises";
 import fetch from "node-fetch";
 import { resolve } from "path";
 import { fileURLToPath } from "url";
+import "dotenv/config";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
@@ -11,14 +12,28 @@ async function copyFromURL(src, dest) {
   await writeFile(dest, str);
 }
 
-console.info("Fetching dataset from dataset.genshin-dictionary.com...");
-
 await mkdir(resolve(__dirname, "../static/dataset/"), { recursive: true });
-await Promise.all([
-  copyFromURL("https://dataset.genshin-dictionary.com/words.json", resolve(__dirname, "../static/dataset/words.json")),
-  copyFromURL("https://dataset.genshin-dictionary.com/words.csv", resolve(__dirname, "../static/dataset/words.csv")),
-  copyFromURL("https://dataset.genshin-dictionary.com/words-sjis.csv", resolve(__dirname, "../static/dataset/words-sjis.csv")),
-  copyFromURL("https://dataset.genshin-dictionary.com/tags.json", resolve(__dirname, "../static/dataset/tags.json")),
-]);
 
-console.info("Fetched dataset from dataset.genshin-dictionary.com.");
+if (process.env.LANGDATA_PATH) {
+  console.info(`Copying dataset from ${process.env.LANGDATA_PATH}`);
+
+  await Promise.all([
+    copyFile(resolve(process.env.LANGDATA_PATH, "dist/words.json"), resolve(__dirname, "../static/dataset/words.json")),
+    copyFile(resolve(process.env.LANGDATA_PATH, "dist/words.csv"), resolve(__dirname, "../static/dataset/words.csv")),
+    copyFile(resolve(process.env.LANGDATA_PATH, "dist/words-sjis.csv"), resolve(__dirname, "../static/dataset/words-sjis.csv")),
+    copyFile(resolve(process.env.LANGDATA_PATH, "dist/tags.json"), resolve(__dirname, "../static/dataset/tags.json")),
+  ]);
+
+  console.info(`Copied dataset from ${process.env.LANGDATA_PATH}`);
+} else {
+  console.info("Fetching dataset from dataset.genshin-dictionary.com...");
+
+  await Promise.all([
+    copyFromURL("https://dataset.genshin-dictionary.com/words.json", resolve(__dirname, "../static/dataset/words.json")),
+    copyFromURL("https://dataset.genshin-dictionary.com/words.csv", resolve(__dirname, "../static/dataset/words.csv")),
+    copyFromURL("https://dataset.genshin-dictionary.com/words-sjis.csv", resolve(__dirname, "../static/dataset/words-sjis.csv")),
+    copyFromURL("https://dataset.genshin-dictionary.com/tags.json", resolve(__dirname, "../static/dataset/tags.json")),
+  ]);
+
+  console.info("Fetched dataset from dataset.genshin-dictionary.com.");
+}
