920d3abdfec8998ce63f6049741d8b26c53fe1e5
Xicri <xicri@proton.me>
2022-11-27 15:42:11 +0900

    Add missing suffix for titles

--- pages/_wordid.vue
+++ pages/_wordid.vue
@@ -41,17 +41,17 @@ export default defineComponent({
     let description;
 
     if (i18n.locale === "en") {
-      title = `"${word.en}" is ` + (word.zhCN ? `"${word.zhCN}" in Chinese` : `${word.ja} in Japanese`);
+      title = `"${word.en}" is ` + (word.zhCN ? `"${word.zhCN}" in Chinese` : `${word.ja} in Japanese`) + ` | ${ i18n.t("siteTitle") }`;
       description = `"${word.en}" is `
                   + (word.zhCN ? `"${word.zhCN}" in Chinese and ` : "")
                   + `"${word.ja}" in Japanese. This website contains English, Chinese, and Japanese translations for terms in Genshin Impact.`;
     } else if (i18n.locale === "ja") {
-      title = `「${word.ja}」は英語で "${word.en}"`;
+      title = `「${word.ja}」は英語で "${word.en}" | ${ i18n.t("siteTitle") }`;
       description = `「${word.ja}」の英語表記は "${word.en}"`
                   + (word.zhCN ? `、中国語表記は「${word.zhCN}」` : "")
                   + " ― このサイトはゲーム「原神」の用語の、日本語・英語・中国語の対訳を掲載しています。";
     } else if (i18n.locale === "zh-CN") {
-      title = word.zhCN ? `"${word.zhCN}"的英语和日语翻译` : `"${word.en}"的日语翻译`;
+      title = (word.zhCN ? `"${word.zhCN}"的英语和日语翻译` : `"${word.en}"的日语翻译`) + ` | ${ i18n.t("siteTitle") }`;
       description = word.zhCN ?
         `"${word.zhCN}"的英语是"${word.en}"，日语是"${word.ja}"。` : // TODO TranslationChanged
         `"${word.en}"的日语是"${word.ja}"。`;
