3c2d3730199951eee251a2ce5c664dd95991c2a5
Xicri <xicri@proton.me>
2023-02-25 16:53:13 +0900

    feat: Next ― migrate `<head>`

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,60 +7,6 @@ export default async () => {
     components: true,
     modern: "client",
 
-    head() {
-      if (!this.$nuxtI18nHead) {
-        return {};
-      }
-
-      const { htmlAttrs, meta, link } = this.$nuxtI18nHead({ addSeoAttributes: true });
-      const { lang } = htmlAttrs;
-
-      let siteName;
-      let description;
-
-      if (lang === "zh-CN") {
-        siteName = "原神中英日辞典";
-        description = "一个在线的中英日三语原神游戏用语辞典";
-      } else if (lang === "ja") {
-        siteName = "原神英語・中国語辞典";
-        description = "原神の固有名詞等の英語表記、及び中国語表記の一覧を掲載しています。";
-      } else {
-        siteName = "Genshin Dictionary";
-        description = "An online English-Chinese-Japanese dictionary for terms in Genshin Impact";
-      }
-
-      return {
-        htmlAttrs,
-        meta: [
-          { charset: "utf-8" },
-          { name: "viewport", content: "width=device-width, initial-scale=1" },
-          { name: "format-detection", content: "telephone=no" },
-          { hid: "description", name: "description", content: description },
-          { hid: "og:description", property: "og:description", content: description },
-          { hid: "og:type", property: "og:type", content: "website" },
-          { hid: "og:site_name", property: "og:site_name", content: siteName },
-          { hid: "twitter:card", property: "twitter:card", content: "summary" },
-          { hid: "twitter:site", property: "twitter:site", content: "@xicri_gi" },
-          { hid: "twitter:creator", property: "twitter:creator", content: "@xicri_gi" },
-          { hid: "google-site-verification", name: "google-site-verification", content: "fPZCIib8QFE52LeBEGqBoapTwL6v9vqHl9lKqcreMDQ" },
-          ...meta,
-        ],
-        link: [
-          { rel: "icon", href: "/images/favicon.svg", type: "image/svg+xml" },
-          ...link,
-        ],
-        script: [
-          ...(process.env.SERVER_ENV === "production" ? [{
-            hid: "cloudflare-wa",
-            src: "https://static.cloudflareinsights.com/beacon.min.js",
-            "data-cf-beacon": "{\"token\": \"1f401150384f4aaa9d14b208aac9fdba\"}",
-            defer: true,
-            body: true,
-          }] : []),
-        ],
-      };
-    },
-
     router: {
       trailingSlash: true,
       middleware: [
@@ -83,8 +29,6 @@ export default async () => {
       vueI18n: {
         messages: {
           en: {
-            siteTitle: "Genshin Dictionary",
-            indexTitleDesc: "An online English-Chinese-Japanese dictionary of the terms in Genshin Impact",
             aboutTitle: "About this website",
             aboutDescription: "About Genshin Dictionary. This website is an online English-Chinese-Japanese dictionary of the terms in Genshin Impact.",
             historyTitle: "Update History",
@@ -94,8 +38,6 @@ export default async () => {
             langNameZhCN: "Chinese",
           },
           ja: {
-            siteTitle: "原神 英語・中国語辞典",
-            indexTitleDesc: "原神の固有名詞等の英語表記、及び中国語表記の一覧を掲載しています。",
             aboutTitle: "このサイトについて",
             aboutDescription: "原神英語・中国語辞典についての説明です。このサイトは PC・スマートフォン・プレイステーション4/5用ゲーム「原神」で用いられる固有名詞等の日本語・英語・中国語対訳表です。",
             historyTitle: "更新履歴",
@@ -105,8 +47,6 @@ export default async () => {
             langNameZhCN: "中国語",
           },
           "zh-CN": {
-            siteTitle: "原神中英日辞典",
-            indexTitleDesc: "一个在线的中英日三语原神游戏用语辞典",
             aboutTitle: "关于本网站",
             aboutDescription: "关于原神中英日辞典。本网站是一个在线的中英日三语原神游戏用语辞典。",
             historyTitle: "更新记录",
