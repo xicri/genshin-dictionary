8c8625af66374979d571e455d4f94dc6f89e4687
Xicri <xicri@proton.me>
2023-02-25 16:19:33 +0900

    feat: Next ― migrate index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,32 @@
-<template>
-  <word-list />
-</template>
+import Head from "next/head";
+import { WordList } from "@/components/WordList";
+import { validateLocale } from "@/libs/i18n";
+import type { GetServerSideProps } from "next";
+import type { Locale } from "@/types";
 
-<script>
-import { defineComponent, useContext, useMeta } from "@nuxtjs/composition-api";
+type Props = {
+  locale: Locale,
+};
 
-export default defineComponent({
-  setup() {
-    const { i18n } = useContext();
-    const title = `${ i18n.t("siteTitle") } ― ${ i18n.t("indexTitleDesc") }`;
+export default function Index({ locale }: Props): JSX.Element {
+  return (
+    <>
+      <Head>
+        {/* Same as default title & description defined in _app.tsx:
 
-    useMeta({
-      title,
-      meta: [
-        { hid: "og:title", property: "og:title", content: title },
-      ],
-    });
+          <title>{`${ t("siteTitle") } ― ${ t("titleDesc") }`}</title>
+          <meta property="og:title" content={`${ t("siteTitle") } ― ${ t("titleDesc") }`} />
+          <meta name="description" content={ t("defaultDescription") } />
+          <meta property="og:description" content={ t("defaultDescription") } />
+        */}
+      </Head>
+      <WordList locale={locale} />
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ locale }): Promise<{ props: Props }> => ({
+  props: {
+    locale: validateLocale(locale),
   },
-  head: {}, // empty head required
 });
-</script>
