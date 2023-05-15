9b8332e16527954849c951204b105d6c69a18469
Xicri <xicri@proton.me>
2023-05-15 11:04:11 +0900

    test: add Visual Regression Test by reg-suit

---
+++ scripts/vrt-reg-cleanup-r2.mjs
@@ -0,0 +1,26 @@
+//
+// Delete old reports with same Git hashes as current production and PR from R2
+//
+import { DeleteObjectsCommand, ListObjectsCommand, S3Client } from "@aws-sdk/client-s3";
+
+const s3 = new S3Client({
+  region: "auto",
+  endpoint: "https://8306a13fafb28ed9cc9c2effe76f7830.r2.cloudflarestorage.com",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+const { Contents: files } = await s3.send(new ListObjectsCommand({
+  Bucket: "genshin-dictionary-reg-suit",
+}));
+
+await s3.send(new DeleteObjectsCommand({
+  Bucket: "genshin-dictionary-reg-suit",
+  Delete: {
+    Objects: files
+      .map(file => ({ Key: file.Key }))
+      .filter(file => file.Key.startsWith(process.env.HASH_PROD) || file.Key.startsWith(process.env.HASH_PR)),
+    Quiet: false,
+  },
+}));
