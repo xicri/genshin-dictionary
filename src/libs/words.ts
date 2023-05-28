da82d4af7ab12dee22472161a10d7e3305988bee
Xicri <xicri@proton.me>
2023-05-28 19:39:09 +0900

    refactor: move `candidate()` & `CandidateString` class to words.ts

--- src/libs/words.ts
+++ src/libs/words.ts
@@ -1,8 +1,48 @@
-import { candidate } from "@/libs/utils";
 import type { BuiltWord, Locale, SearchConditions } from "@/types";
 
 import _allWords from "../../public/dataset/words.json";
 const allWords = _allWords as BuiltWord[];
+
+class CandidateString {
+  constructor(private candidate: string|undefined) {}
+
+  private normalize(str: string): string {
+    return str
+      // replace Katakana with Hiragana
+      .replace(/[ァ-ヴ]/g, (str) => String.fromCharCode(str.charCodeAt(0) - 0x60))
+      // ignore symbols
+      // e.g. search query: "勤労の導き" -> result: "「勤労」の導き"
+      // e.g. search query: "氷元素" -> result: "氷 (元素)"
+      // e.g. search query: "ジングンヒルド" -> result: "ジン・グンヒルド"
+      .replace(/[（）()「」『』<>＜＞《》'"、。,.，．・!?！？ 　]/g, "")
+      .replace(/ゔぁ/g, "ば")
+      .replace(/ゔぃ/g, "び")
+      .replace(/ゔぇ/g, "べ")
+      .replace(/ゔぉ/g, "ぼ")
+      .replace(/ゔ/g, "ぶ") // You need to replace after ゔぇ & ゔぉ, or ゔぇ & ゔぉ would be converted to ぶぇ & ぶぉ.
+      .toLowerCase();
+  }
+
+  equals(searchElement: string): boolean {
+    if (!this.candidate) {
+      return false;
+    }
+
+    return this.normalize(this.candidate) === this.normalize(searchElement);
+  }
+
+  includes(searchElement: string): boolean {
+    if (!this.candidate) {
+      return false;
+    }
+
+    const candidate = this.normalize(this.candidate);
+    const _searchElement = this.normalize(searchElement);
+    return candidate.includes(_searchElement);
+  }
+}
+
+const candidate = (str: string|undefined): CandidateString => new CandidateString(str);
 
 type GetWordsReturn = {
   words: BuiltWord[],
