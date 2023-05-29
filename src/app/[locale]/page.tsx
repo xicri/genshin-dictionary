11a12030ae5df1dfc69b79e39dc74970501bdb49
Xicri <xicri@proton.me>
2023-05-29 11:08:02 +0900

    refactor: migrate index (/[locale]/) to App Router

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,32 +1,57 @@
-import Head from "next/head";
 import { WordList } from "@/components/WordList";
-import { validateLocale } from "@/libs/i18n";
-import type { GetStaticProps, InferGetStaticPropsType } from "next";
-import type { Locale } from "@/types";
+import { I18n } from "@/libs/i18n";
+import { generateAlternates } from "@/libs/meta";
+import { locales } from "@/config";
+import type { Locale, Metadata } from "@/types";
 
 type Props = {
-  locale: Locale,
+  params: { locale: Locale };
 };
 
-export const getStaticProps: GetStaticProps<Props> = async ({ locale }) => ({
-  props: {
-    locale: validateLocale(locale),
-  },
-});
+export async function generateMetaData({ params: { locale }}: Props): Promise<Metadata> {
+  //
+  // i18n
+  //
+  const i18n = new I18n(locale, {
+    en: {
+      titleDesc: "An online English-Chinese-Japanese dictionary of the terms in Genshin Impact",
+      description: "An online English-Chinese-Japanese dictionary for terms in Genshin Impact",
+    },
+    ja: {
+      titleDesc: "原神の固有名詞等の英語表記、及び中国語表記の一覧を掲載しています。",
+      description: "原神の固有名詞等の英語表記、及び中国語表記の一覧を掲載しています。",
+    },
+    "zh-CN": {
+      titleDesc: "一个在线的中英日三语原神游戏用语辞典",
+      description: "一个在线的中英日三语原神游戏用语辞典",
+    },
+  });
 
-export default function Index({ locale }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
-  return (
-    <>
-      <Head>
-        {/* Same as default title & description defined in _app.tsx:
+  const title = `${ i18n.t("siteTitle") } ― ${ i18n.t("titleDesc") }`;
+  const description = i18n.t("description");
+  const { canonical, languages } = generateAlternates("/", locale);
 
-          <title>{`${ t("siteTitle") } ― ${ t("titleDesc") }`}</title>
-          <meta property="og:title" content={`${ t("siteTitle") } ― ${ t("titleDesc") }`} />
-          <meta name="description" content={ t("defaultDescription") } />
-          <meta property="og:description" content={ t("defaultDescription") } />
-        */}
-      </Head>
-      <WordList locale={locale} />
-    </>
-  );
+  return {
+    title: {
+      absolute: title,
+    },
+    description,
+    alternates: {
+      canonical,
+      languages,
+    },
+    openGraph: {
+      title,
+      description,
+      url: canonical,
+    },
+  };
+}
+
+export async function generateStaticParams(): Promise<{ locale: Locale; }[]> {
+  return locales.map(locale => ({ locale }));
+}
+
+export default function Index({ params: { locale }}: Props): JSX.Element {
+  return (<WordList locale={locale} />);
 }
--- src/libs/translations.ts
+++ src/libs/translations.ts
@@ -3,14 +3,11 @@ import type { Translations } from "@/types";
 export const translations: Translations = {
   en: {
     siteTitle: "Genshin Dictionary",
-    titleDesc: "An online English-Chinese-Japanese dictionary of the terms in Genshin Impact",
   },
   ja: {
     siteTitle: "原神 英語・中国語辞典",
-    titleDesc: "原神の固有名詞等の英語表記、及び中国語表記の一覧を掲載しています。",
   },
   "zh-CN": {
     siteTitle: "原神中英日辞典",
-    titleDesc: "一个在线的中英日三语原神游戏用语辞典",
   },
 };
