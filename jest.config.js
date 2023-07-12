ffb69bc7d0067338710dd2389b84a93ad6a24474
Xicri <xicri@proton.me>
2023-07-12 09:02:05 +0900

    test: remove no-longer required `babel-jest`

--- jest.config.js
+++ jest.config.js
@@ -2,17 +2,10 @@ export default {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
     "^~/(.*)$": "<rootDir>/$1",
-    "^vue$": "vue/dist/vue.common.js",
   },
   moduleFileExtensions: [
     "js",
     "vue",
     "json",
   ],
-  transform: {
-    "^.+\\.js$": "babel-jest",
-    // TODO Temporarily disable vue-jest because it still uses deprecated `babel-core` package and it prevents running Jest properly.
-    // see: https://stackoverflow.com/questions/56289348/cannot-find-module-babel-core-but-babel-core-is-installed
-    // ".*\\.(vue)$": "vue-jest",
-  },
 };
