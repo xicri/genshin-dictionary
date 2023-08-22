26f7711bfc610dc1ee8aa079262b8f0f55d747b4
Xicri <xicri@proton.me>
2023-08-22 09:43:14 +0900

    chore: use xicri/nuxt+ts ESLint ruleset

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,44 +2,12 @@
 
 module.exports = {
   root: true,
-  extends: "xicri/vue+js",
+  extends: "xicri/nuxt+ts",
 
-  globals: {
-    computed: "readonly",
-    createError: "readonly",
-    defineEventHandler: "readonly",
-    defineNuxtConfig: "readonly",
-    defineNuxtRouteMiddleware: "readonly",
-    navigateTo: "readonly",
-    nextTick: "readonly",
-    onMounted: "readonly",
-    onUpdated: "readonly",
-    useContext: "readonly",
-    useHead: "readonly",
-    useLazyAsyncData: "readonly",
-    useNuxtApp: "readonly",
-    useRoute: "readonly",
-    useRuntimeConfig: "readonly",
-    ref: "readonly",
-
-    // nuxt/i18n
-    useI18n: "readonly",
-    useLocaleHead: "readonly",
-    useLocalePath: "readonly",
-    useSwitchLocalePath: "readonly",
   },
 
   rules: {
-    "vue/html-self-closing": [ "error", {
-      html: {
-        void: "always",
-        normal: "always",
-        component: "always",
-      },
-    }],
     "no-unused-vars": "off",
-    "vue/multi-word-component-names": "off",
-    "vue/no-v-html": "off",
   },
 
   overrides: [
