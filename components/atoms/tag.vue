584c4071ef4ac0b92876e54673bc57170c9bc039
Xicri <xicri@proton.me>
2023-09-16 08:44:01 +0900

    refactor: add button to `<tag>`

--- components/atoms/tag.vue
+++ components/atoms/tag.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="tag">
     <span>{{ tagName }}</span>
+    <span v-if="button" class="tag__button" @click="emit('buttonClick')">
+      {{ button }}
+    </span>
   </div>
 </template>
 
@@ -15,15 +18,41 @@ const props = defineProps({
     type: String as PropType<TagID>,
     required: true,
   },
+  button: {
+    type: String,
+    required: false,
+    default: undefined,
+  },
+  // required to be defined here to check if these events are listened by parent components
+  onButtonClick: {
+    type: Function,
+    required: false,
+    default: undefined,
+  },
 });
 
+const emit = defineEmits([ "buttonClick" ]);
+
 const tagName = allTags[props.tagid][locale.value];
+
+let buttonPointer: "default" | "pointer";
+
+if (props.onButtonClick) {
+  buttonPointer = "pointer";
+} else { // @buttonClick is not defined
+  buttonPointer = "default";
+}
 </script>
 
 <style lang="scss" scoped>
 @use "~/assets/styles/variables.scss" as vars;
 
 .tag {
+  display: flex;
+  flex-shrink: 0;
+  align-items: center;
+  gap: 0.34em;
+
   border-width: 2px;
   border-style: solid;
   border-radius: 6px;
@@ -36,5 +65,10 @@ const tagName = allTags[props.tagid][locale.value];
 
   color: vars.$color-dark;
   background-color: vars.$color-lightest;
+
+  &__button {
+    font-weight: 1000;
+    cursor: v-bind("buttonPointer");
+  }
 }
 </style>
