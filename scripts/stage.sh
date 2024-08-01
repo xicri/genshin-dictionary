948880dc8f5a13d83e47e7f0ae17ecfc900d4830
Xicri <107226297+xicri@users.noreply.github.com>
2024-08-02 05:28:07 +0900

    ci: remove deployment scripts for Fly.io

--- scripts/stage.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -eu
-
-type flyctl > /dev/null
-
-cp Dockerfile Dockerfile_staging
-sed -i -e 's/production/staging/g' Dockerfile_staging
-flyctl deploy --remote-only --app=genshin-dictionary-staging --dockerfile=Dockerfile_staging
