d614d0c1d9b4efa2e41aa1ea426a4989b3baf03f
Xicri <107226297+xicri@users.noreply.github.com>
2024-09-16 14:33:02 +0900

    refactor: use Cloudflare Pages' _headers file

--- server/middleware/headers.ts
+++
@@ -1,7 +0,0 @@
-export default defineEventHandler(({ res }) => {
-  if (res) {
-    res.setHeader("Cross-Origin-Resource-Policy", "same-site");
-    res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-    res.setHeader("X-Content-Type-Options", "nosniff");
-  }
-});
