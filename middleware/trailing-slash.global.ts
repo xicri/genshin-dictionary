318e34c158131eba352535498af5a5e7c48e2235
Xicri <107226297+xicri@users.noreply.github.com>
2024-09-30 12:35:04 +0900

    refactor: remove unnecessary redirection middleware to remove trailing slashes

--- middleware/trailing-slash.global.ts
+++
@@ -1,5 +0,0 @@
-export default defineNuxtRouteMiddleware((to) => {
-  if (to.path.endsWith("/") && to.path !== "/") {
-    return navigateTo(to.path.replace(/\/+$/, ""), { redirectCode: 301 });
-  }
-});
