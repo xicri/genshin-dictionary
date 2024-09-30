b2819f66aa973adc21f5eed7e90224cf8b732b6b
Xicri <107226297+xicri@users.noreply.github.com>
2024-09-30 12:45:09 +0900

    refactor: use Cloudflare Pages' _redirects for renamed slugs of the words

---
+++ scripts/build-redirects.ts
@@ -0,0 +1,17 @@
+import { writeFile } from "node:fs/promises";
+import { join } from "node:path";
+import tagRedirects from "../dataset/redirect/tags.json";
+import wordRedirects from "../dataset/redirect/words.json";
+
+const cfPagesRedirectConf: string = [
+  ...(
+    Object.entries(wordRedirects)
+      .map(([ oldSlug, newSlug ]) => `/:lang/${ oldSlug } /:lang/${ newSlug } 301`)
+  ),
+  ...(
+    Object.entries(tagRedirects)
+      .map(([ oldSlug, newSlug ]) => `/:lang/tags/${ oldSlug } /:lang/tags/${ newSlug } 301`)
+  ),
+].join("\n");
+
+await writeFile(join(import.meta.dirname, "../public/_redirects"), cfPagesRedirectConf);
