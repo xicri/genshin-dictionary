3473b8141db42f08ab3f0994f53f4ca3ca1cbb4d
Xicri <107226297+xicri@users.noreply.github.com>
2025-05-07 07:42:07 +0900

    refactor: prepare scripts/build.ts to add another build process (2)

--- scripts/build.ts
+++ scripts/build.ts
@@ -3,15 +3,21 @@ import { join } from "node:path";
 import tagRedirects from "../dataset/redirect/tags.json";
 import wordRedirects from "../dataset/redirect/words.json";
 
-const cfPagesRedirectConf: string = [
-  ...(
-    Object.entries(wordRedirects)
-      .map(([ oldSlug, newSlug ]) => `/:lang/${ oldSlug } /:lang/${ newSlug } 301`)
-  ),
-  ...(
-    Object.entries(tagRedirects)
-      .map(([ oldSlug, newSlug ]) => `/:lang/tags/${ oldSlug } /:lang/tags/${ newSlug } 301`)
-  ),
-].join("\n");
+const buildRedirectConf = async () => {
+  const cfPagesRedirectConf: string = [
+    ...(
+      Object.entries(wordRedirects)
+        .map(([ oldSlug, newSlug ]) => `/:lang/${ oldSlug } /:lang/${ newSlug } 301`)
+    ),
+    ...(
+      Object.entries(tagRedirects)
+        .map(([ oldSlug, newSlug ]) => `/:lang/tags/${ oldSlug } /:lang/tags/${ newSlug } 301`)
+    ),
+  ].join("\n");
 
-await writeFile(join(import.meta.dirname, "../public/_redirects"), cfPagesRedirectConf);
+  await writeFile(join(import.meta.dirname, "../public/_redirects"), cfPagesRedirectConf);
+};
+
+await Promise.all([
+  buildRedirectConf(),
+])
