e4461541242cd02936bcf59b811c15050d575e8a
Xicri <xicri@proton.me>
2026-01-17 07:24:41 +0900

    refactor: migrate HTTP headers for SvelteKit

--- server/middleware/headers.ts
+++
@@ -1,5 +0,0 @@
-export default defineEventHandler((event) => {
-  setHeader(event, "Cross-Origin-Resource-Policy", "same-site");
-  setHeader(event, "Cross-Origin-Opener-Policy", "same-origin");
-  setHeader(event, "X-Content-Type-Options", "nosniff");
-});
