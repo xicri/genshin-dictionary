4f72f615b4a42b43e8ebb1a92bfa1be35a8af7b7
Xicri <xicri@proton.me>
2026-01-17 06:13:58 +0900

    refactor: remove no longer required tmp/locales.json

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,6 +1,5 @@
-import { mkdir, writeFile } from "node:fs/promises";
+import { writeFile } from "node:fs/promises";
 import { join } from "node:path";
-import { locales } from "../nuxt.config.ts";
 import tagRedirects from "../dataset/redirect/tags.json";
 import wordRedirects from "../dataset/redirect/words.json";
 
@@ -19,18 +18,4 @@ const buildRedirectConf = async () => {
   await writeFile(join(import.meta.dirname, "../_redirects"), cfPagesRedirectConf);
 };
 
-const buildLocaleJson = async () => {
-  const tmpDirPath = join(import.meta.dirname, "../tmp/");
-  const localeCodes = locales.map((locale) => locale.code);
-
-  await mkdir(tmpDirPath, { recursive: true });
-  await writeFile(
-    join(tmpDirPath, "locales.json"),
-    JSON.stringify(localeCodes, undefined, 2),
-  );
-};
-
-await Promise.all([
-  buildRedirectConf(),
-  buildLocaleJson(),
-]);
+await buildRedirectConf();
