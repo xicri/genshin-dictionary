cb565906529267b7058fc05e571920255e9f31b6
Xicri <xicri@proton.me>
2026-01-18 12:59:52 +0900

    refactor: avoid `{@html}` for the words with Pinyin

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -47,23 +47,3 @@ export const sleep = async (ms: number): Promise<void> =>
   new Promise((resolve) =>
     setTimeout(() => resolve(undefined), ms),
   );
-
-export const escapeHtmlString = (html: string): string => {
-  const map = {
-    "&": "&amp;",
-    "<": "&lt;",
-    ">": "&gt;",
-  };
-
-  return html.replace(/[&<>]/g, (charToEscape) => {
-    if (
-      charToEscape === "&"
-      || charToEscape === "<"
-      || charToEscape === ">"
-    ) {
-      return map[charToEscape];
-    } else {
-      return charToEscape;
-    }
-  });
-};
