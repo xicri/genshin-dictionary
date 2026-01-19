5b4dc5a326a69cb6a5190304950cf2761af024d7
Xicri <xicri@proton.me>
2026-01-19 13:23:52 +0900

    refactor: migrate robots.txt from Nuxt to SvelteKit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,6 @@ export default defineNuxtConfig({
   css: [ "~/assets/styles/global.scss" ],
 
   modules: [
-    "@nuxtjs/robots",
     "@pinia/nuxt",
   ],
-
-  // For nuxt-simple-robots and nuxt-simple-sitemap
-  site: {
-    url: "https://genshin-dictionary.com",
-    indexable: process.env.SERVER_ENV === "production",
-  },
 });
