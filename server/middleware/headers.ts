bd08a257a3b00bf09a34ded67557e748ad2a69a6
Xicri <xicri@proton.me>
2026-01-19 10:39:27 +0900

    feat: use secure `Cross-Origin-Resource-Policy: same-origin`

--- server/middleware/headers.ts
+++ server/middleware/headers.ts
@@ -1,5 +1,5 @@
 export default defineEventHandler((event) => {
-  setHeader(event, "Cross-Origin-Resource-Policy", "same-site");
+  setHeader(event, "Cross-Origin-Resource-Policy", "same-origin");
   setHeader(event, "Cross-Origin-Opener-Policy", "same-origin");
   setHeader(event, "X-Content-Type-Options", "nosniff");
 });
