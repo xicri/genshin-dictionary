d635d23183e664e7c71fcd67a8f784a852f189cc
Xicri <xicri@proton.me>
2026-01-19 13:23:52 +0900

    chore: migrate from robots.txt to `<meta name="robots">` for non-prod envs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,14 +2,4 @@ import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
   css: [ "~/assets/styles/global.scss" ],
-
-  modules: [
-    "@nuxtjs/robots",
-  ],
-
-  // For nuxt-simple-robots and nuxt-simple-sitemap
-  site: {
-    url: "https://genshin-dictionary.com",
-    indexable: process.env.SERVER_ENV === "production",
-  },
 });
