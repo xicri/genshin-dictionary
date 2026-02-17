69a40cf3db5b64b756b1b32cda3be9933fa9c1f4
Xicri <xicri@proton.me>
2026-02-17 14:38:30 +0900

    perf: move processes to server side

--- src/routes/tags/[slug]/+page.server.ts
+++ src/routes/tags/[slug]/+page.server.ts
@@ -1,9 +1,28 @@
 import { error } from "@sveltejs/kit";
+import { m } from "$lib/paraglide/messages.js";
+import { getLocale } from "$lib/paraglide/runtime.js";
+import tags from "$lib/dataset/tags.json";
+import type { TagID } from "$lib/types.ts";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params }) => {
   if (params.slug) {
+    const getTagIdFromParams = (): TagID => {
+      const tagID = params.slug;
+
+      if (!Object.keys(tags).includes(tagID)) { // unexpected tagID given
+        error(404, "Not found");
+      }
+
+      return tagID as TagID;
+    };
+
+    const locale = getLocale();
+    const tagSlug = getTagIdFromParams();
+    const title = `${ tags[tagSlug].title[locale] } | ${ m.siteTitle() }`;
+
     return {
+      title,
       slug: params.slug,
     };
   } else {
--- src/routes/tags/[slug]/+page.svelte
+++ src/routes/tags/[slug]/+page.svelte
@@ -1,32 +1,14 @@
 <script lang="ts">
-  import { error } from "@sveltejs/kit";
   import WordList from "$lib/components/WordList.svelte";
-  import { m } from "$lib/paraglide/messages.js";
-  import { getLocale } from "$lib/paraglide/runtime.js";
-  import tags from "$lib/dataset/tags.json";
-  import type { TagID } from "$lib/types.ts";
   import type { PageProps } from "./$types";
 
   let { data }: PageProps = $props();
 
-  const getTagIdFromParams = (): TagID => {
-    const tagID = data.slug;
-
-    if (!Object.keys(tags).includes(tagID)) { // unexpected tagID given
-      error(404, "Not found");
-    }
-
-    return tagID as TagID;
-  };
-
-  const locale = getLocale();
-  const tagSlug = getTagIdFromParams();
-  const title = `${ tags[tagSlug].title[locale] } | ${ m.siteTitle() }`;
 </script>
 
 <svelte:head>
-  <title>{title}</title>
-  <meta property="og:title" content={title} />
+  <title>{data.title}</title>
+  <meta property="og:title" content={data.title} />
 </svelte:head>
 
-<WordList {tagSlug} />
+<WordList tagSlug={data.slug} />
